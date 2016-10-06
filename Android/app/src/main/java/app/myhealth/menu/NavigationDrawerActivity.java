4ee66779269e983ccf33bdb0421f713b240d1b93
Koen Berghuis <koenberghuis@gmail.com>
2016-10-06 10:39:09 +0200

    In progress

--- Android/app/src/main/java/app/myhealth/menu/NavigationDrawerActivity.java
+++ Android/app/src/main/java/app/myhealth/menu/NavigationDrawerActivity.java
@@ -30,14 +30,16 @@ public class NavigationDrawerActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        //TODO: remove this?
+       /** FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });**/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
